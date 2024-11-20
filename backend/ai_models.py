92964f2e6267cb75c03b74a4ff16c9e63270c1c0
Ashish Soni <ashish.soni2091@gmail.com>
2024-11-20 14:16:56 +0000

    Refactor AI model configurations to use Jina and SambaNovaCloud, updating embedding and LLM models

--- backend/ai_models.py
+++ backend/ai_models.py
@@ -1,23 +1,43 @@
 from dotenv import dotenv_values
 
-import google.generativeai as genai
-
-from llama_index.llms.gemini import Gemini
-from llama_index.embeddings.gemini import GeminiEmbedding
+from llama_index.embeddings.jinaai import JinaEmbedding
+from llama_index.postprocessor.jinaai_rerank import JinaRerank
+from llama_index.llms.sambanovacloud import SambaNovaCloud
 
 config = dotenv_values("backend/.env")
 
 # MODELS
-EMBEDDING_MODEL: str = "models/text-embedding-004"
+EMBEDDING_MODEL: str = "jina-embeddings-v3"
 
-LLM_MODEL: str = "models/gemini-1.5-pro"
+LLM_MODEL: str = "Meta-Llama-3.2-3B-Instruct"
 
-# text-embedding-004
-embedding_model = GeminiEmbedding(api_key=config["GEMINI_API_KEY"],
-                                  model=EMBEDDING_MODEL,
-                                  truncate="END")
+RERANK_MODEL: str = "jina-reranker-v2-base-multilingual"
 
-# gemini-1.5-pro
-llm = Gemini(api_key=config["GEMINI_API_KEY"],
+# text embeddings from pdf
+text_embed_model = JinaEmbedding(
+    api_key=config["JINA_API_KEY"],
+    model=EMBEDDING_MODEL,
+    task="retrieval.passage",
+    dimension=512,
+    late_chunking=True,
+    embedding_type="float"
+    )
+
+# text embeddings from user input
+query_embed_model = JinaEmbedding(
+    api_key=config["JINA_API_KEY"],
+    model=EMBEDDING_MODEL,
+    task="retrieval.query",
+    dimension=512,
+    late_chunking=False,
+    embedding_type="float"
+    )
+
+# Meta-Llama-3.2-3B-Instruct
+llm = SambaNovaCloud(api_key=config["SAMBANOVA_API_KEY"],
              model=LLM_MODEL,
-             temperature=0.2)
+             max_tokens=1024,
+             temperature=0.2,
+             top_k=1,
+             top_p=0.9
+             )
